f0d9e2372c7dade64122495d1bde860b23d64994
Ben Frith <bpfrith@gmail.com>
2017-01-22 23:20:28 +0000

    having trouble with json, had hoped using gson would help.

--- ToDo_List/app/src/main/java/example/com/codeclan/example/todo_list/SavedListPreferences.java
+++ ToDo_List/app/src/main/java/example/com/codeclan/example/todo_list/SavedListPreferences.java
@@ -1,8 +1,35 @@
 package example.com.codeclan.example.todo_list;
 
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+import com.google.gson.Gson;
+import com.gson.InstanceCreator;
+
 /**
  * Created by user on 22/01/2017.
  */
 
 public class SavedListPreferences {
+
+    private static final String PREF_SAVEDLIST = "savedList";
+
+    public static void setStoredList(Context context, List list){
+
+        Gson gson = new Gson();
+        String listJSON = gson.toJson(list);
+
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PERF_SAVEDLIST, listJSON)
+                .apply();
+    }
+
+    public static List getStoredList(Context context){
+        String listJSON = PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_SAVEDLIST, null);
+        Gson gson = new Gson();
+        List list = gson.fromJson(listJSON, List.class);
+        return list
+    }
 }
